8514390e0ff509a7289da745b713de0d36fc6f77
Ajay <ajay@terem.com.au>
2021-05-29 14:18:28 +1000

    moved to 4.50.00

--- build_apigee_base.sh
+++ build_apigee_base.sh
@@ -4,4 +4,4 @@ read -p 'License path: ' license
 read -p 'Username (software.apigee.com): ' user
 read -sp 'Password (software.apigee.com): ' pwd
 
-docker build -t apigee_base:1709 -f apigee_base/Dockerfile . --build-arg ftp_user=$user --build-arg ftp_pwd=$pwd --build-arg license_path=$license --no-cache
+docker build -t apigee_base:5000 -f apigee_base/Dockerfile . --build-arg ftp_user=$user --build-arg ftp_pwd=$pwd --build-arg license_path=$license --no-cache
